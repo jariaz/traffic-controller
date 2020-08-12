b1f52589752d1eea9b665ebbcb8c7eab59665e5b
Joseph Ariaz <josephariaz@gmail.com>
2020-08-11 21:45:45 -0700

    Made the router domain specific, each site can now have its own routing rules

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import defaultRouter from './router' // router
+import siteARouter from './router/sitea'
+import siteBRouter from './router/siteb'
+import defaultStore from './store' // store
 
 Vue.config.productionTip = false
 
+const domain = window.location.hostname;
+
+const router = () => {
+  let routes;
+  if (domain === 'local.site-a.com') {
+    routes = siteARouter;
+  } else if (domain === 'local.site-b.com') {
+    routes = siteBRouter;
+  } else {
+    routes = defaultRouter;
+  }
+  return routes;
+}
+
+const store = () => {
+  // let stores;
+  return defaultStore
+}
+
 new Vue({
-  router,
-  store,
+  router: router(),
+  store: store(),
   render: h => h(App)
 }).$mount('#app')
